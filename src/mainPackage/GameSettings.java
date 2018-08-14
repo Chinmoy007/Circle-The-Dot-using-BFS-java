dad9e71df298f9a34f35c1c03db9420be763bd6c
Chinmoy007 <chinmoyacharjee15@gmail.com>
2018-08-15 01:46:14 +0600

    window created

---
+++ src/mainPackage/GameSettings.java
@@ -0,0 +1,13 @@
+package mainPackage;
+
+public class GameSettings {
+	// game properties
+	public static String title = "Circle-The-Dot-AI";
+	public static int height = 600;
+	public static int width = 900;
+
+	// location on monitor
+	public static int loacationX = 500;
+	public static int locationY = 200;
+
+}
